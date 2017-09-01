62bf0b26c55228f281bf12e1d696ad4ec39a49bf
jonnyalexbh <jonnyalex.bh@gmail.com>
2017-09-01 16:05:50 -0500

    home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// Componentes
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/home.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'home',
+  templateUrl: '../views/home.component.html'
+})
+export class HomeComponent{
+  public titulo: string;
+
+  constructor(){
+    this.titulo = 'Webapp de productos con Angular 4';
+  }
+
+  ngOnInit(){
+    console.log('Se ha cargado el componente home.component.ts');
+  }
+}
