5379b216c7609888dbae725791a40c3ac40eab42
jonnyalexbh <jonnyalex.bh@gmail.com>
2017-09-05 18:38:00 -0500

    upload images

--- src/app/components/producto-add.component.ts
+++ src/app/components/producto-add.component.ts
@@ -13,6 +13,8 @@ import { GLOBAL } from '../services/global';
 export class ProductoAddComponent{
   public titulo: string;
   public producto: Producto;
+  public filesToUpload;
+  public resultUpload;
 
   constructor(
     private _productoService: ProductoService,
@@ -29,20 +31,42 @@ export class ProductoAddComponent{
 
   onSubmit(){
     console.log(this.producto);
+
+    if(this.filesToUpload && this.filesToUpload.length >= 1){
+      this._productoService.makeFileRequest(GLOBAL.url+'upload-file', [], this.filesToUpload).then((result) => {
+        console.log(result);
+
+        this.resultUpload = result;
+        this.producto.imagen = this.resultUpload.filename;
+        this.saveProducto();
+
+      }, (error) =>{
+        console.log(error);
+      });
+    }else{
+      this.saveProducto();
+    }
+
+  }
+
+  saveProducto(){
     this._productoService.addProducto(this.producto).subscribe(
       response => {
-
         if(response.code == 200){
           this._router.navigate(['/productos']);
         }else{
           console.log(response);
         }
-
       },
       error => {
         console.log(<any>error);
       }
-    )
+    );
+  }
+
+  fileChangeEvent(fileInput: any){
+    this.filesToUpload = <Array<File>>fileInput.target.files;
+    console.log(this.filesToUpload);
   }
 
 }
--- src/app/services/producto.service.ts
+++ src/app/services/producto.service.ts
@@ -28,4 +28,28 @@ export class ProductoService{
     .map(res => res.json());
   }
 
+  makeFileRequest(url: string, params: Array<string>, files: Array<File>){
+    return new Promise((resolve, reject)=>{
+      var formData: any = new FormData();
+      var xhr = new XMLHttpRequest();
+
+      for(var i = 0; i < files.length; i++){
+        formData.append('uploads[]', files[i], files[i].name);
+      }
+
+      xhr.onreadystatechange = function(){
+        if(xhr.readyState == 4){
+          if(xhr.status == 200){
+            resolve(JSON.parse(xhr.response));
+          }else{
+            reject(xhr.response);
+          }
+        }
+      };
+
+      xhr.open("POST", url, true);
+      xhr.send(formData);
+    });
+  }
+
 }
